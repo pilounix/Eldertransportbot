74a56c2d157e5e2a1e0cbc0c72b2e726de7ecc71
pilounix <40580921+pilounix@users.noreply.github.com>
2019-04-28 17:35:52 +0200

    Add files via upload

---
+++ .gitignore/main.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client();
+
+const prefix = '&';
+
+bot.on('ready', () => {
+
+    console.log('bot is ready with username: ' + bot.user.username)
+    bot.user.setActivity(`&help`, {type: "PLAYING"});
+
+
+});
+
+bot.on('message', msg => {
+	if(msg.content.toLowerCase().startsWith(prefix + 'ping')) {
+		msg.channel.send('Pong! ' + Math.round(bot.ping) + ' ms!')
+	}
+});
+
+
+bot.on('message', msg => {
+	if(msg.content.toLowerCase().startsWith(prefix + 'clear')) {		
+		msg.delete()
+		if(msg.member.hasPermission("MANAGE_MESSAGE")) {
+			msg.channel.fetchMessage()
+				.then(function(list){
+					msg.channel.bulkDelete(list);
+				}, function(err){msg.channel.send("Erreur")})}	
+	}
+});
+
+bot.login("NTcxNzk5MDg0NDIwMzAwODAw.XMWLtg.vEPxOeLe26RGSNVYATmAvFpeesc");
